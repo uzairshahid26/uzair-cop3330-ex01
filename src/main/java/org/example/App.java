e28ac1ed3613e9a309b7b5df3b8c14c46ff7de68
uzairshahid26 <89487085+uzairshahid26@users.noreply.github.com>
2021-08-31 18:44:06 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,24 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 1 Solution
+ *  Copyright 2021 Muhammad Uzair
+ */
+
+
+package org.example;
+import java.util.Scanner;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Hello, What is your name? ");
+        String name = input.next();
+        System.out.println("Hello "+name+", nice to meet you!");
+    }
+
+
+
+
+}
